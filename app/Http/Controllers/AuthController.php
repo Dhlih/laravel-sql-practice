a2faf4105bc7e2a861fb1a7218f1af69b4a58cc6
ifad <yusuffadhlih123@gmail.com>
2026-07-11 21:53:32 +0700

    Membuat AuthController yang berisi fungsi login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $email = $request->input("email");
+        $password = $request->input("password");
+
+        $user = DB::select("SELECT id, email, password FROM users WHERE email = ?", [$email]);
+        if ($user && Hash::check($password, $user[0]->password)) {
+            session(["user_id" => $user[0]->id, "user_email" => $user[0]->email]);
+            return redirect("/dashboard");
+        }
+        return back()->with("error", "Email atau password salah");
+    }
+}
