8de8779bea6cfc6b0ad16223f2933e72c9cb1cdf
ifad <yusuffadhlih123@gmail.com>
2026-07-12 12:24:39 +0700

    Menambahkan endpoint untuk logout dan menampikan halaman dashboard

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+# Authentication routes
 Route::get('/login', function () {
     return view("login");
 });
-
 Route::post('/login', [AuthController::class, "login"]);
+Route::post('/logout', [AuthController::class, "logout"]);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
