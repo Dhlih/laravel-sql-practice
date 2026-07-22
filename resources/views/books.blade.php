3344aba43f15be61974540731a3fc0492cb96acc
ifad <yusuffadhlih123@gmail.com>
2026-07-22 22:48:01 +0700

    Menambahkan tombol penambahan buku dan menyambungkan input judul buku ke controller

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -6,99 +6,42 @@
     <div class="flex items-center justify-between mb-10">
         <h1 class="text-4xl font-bold text-gray-800">Buku</h1>
 
-        <div class="flex items-center gap-4 ">
-            <input type="text" placeholder="Ketik judul buku..."
-                class="max-w-xl w-full bg-white p-2 rounded-lg outline-none">
-            <select class="bg-white p-2 rounded-lg outline-none">
+        <form action="/buku" method="GET" class="max-w-sm w-full flex items-center gap-4 ">
+            <input type="text" name="judul" placeholder="Ketik judul buku..."
+                class="w-full bg-white p-2 rounded-lg outline-none shadow-lg" value="{{ request('judul') }}">
+
+            <select class="bg-white p-2 rounded-lg outline-none shadow-lg">
                 <option value="test">Test</option>
                 <option value="test2">Test 2</option>
                 <option value="test3">Test 3</option>
             </select>
-        </div>
-    </div>
 
-    <div class="flex flex-wrap -m-3 bg-white p-2 rounded-xl shadow-lg ">
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6">
-            <div class="w-full flex flex-col items-center">
-                <img src="book.jpg" class="w-full aspect-[3/4] object-cover rounded-lg" alt="The Alchemist">
-                <div class="mt-2 text-center">
-                    <h3 class="text-lg font-semibold">The Alchemist</h3>
-                    <p class="text-gray-500">Paulo Coelho</p>
-                </div>
-            </div>
-        </div>
+            <button type="submit"
+                class="bg-[#F1E8FD] p-2 px-4 rounded-lg font-semibold shadow-lg hover:bg-[#E5D5FC] transition cursor-pointer">
+                <i data-feather="search"></i>
 
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6">
-            <div class="w-full flex flex-col items-center">
-                <img src="book.jpg" class="w-full aspect-[3/4] object-cover rounded-lg" alt="The Alchemist">
-                <div class="mt-2 text-center">
-                    <h3 class="text-lg font-semibold">The Alchemist</h3>
-                    <p class="text-gray-500">Paulo Coelho</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6">
-            <div class="w-full flex flex-col items-center">
-                <img src="book.jpg" class="w-full aspect-[3/4] object-cover rounded-lg" alt="The Alchemist">
-                <div class="mt-2 text-center">
-                    <h3 class="text-lg font-semibold">The Alchemist</h3>
-                    <p class="text-gray-500">Paulo Coelho</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6">
-            <div class="w-full flex flex-col items-center">
-                <img src="book.jpg" class="w-full aspect-[3/4] object-cover rounded-lg" alt="The Alchemist">
-                <div class="mt-2 text-center">
-                    <h3 class="text-lg font-semibold">The Alchemist</h3>
-                    <p class="text-gray-500">Paulo Coelho</p>
-                </div>
-            </div>
-        </div>
-
-
-
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6">
-            <div class="w-full flex flex-col items-center">
-                <img src="book.jpg" class="w-full aspect-[3/4] object-cover rounded-lg" alt="The Alchemist">
-                <div class="mt-2 text-center">
-                    <h3 class="text-lg font-semibold">The Alchemist</h3>
-                    <p class="text-gray-500">Paulo Coelho</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6 ">
-            <div class="w-full flex flex-col items-center">
-                <img src="book.jpg" class="w-full aspect-[3/4] object-cover rounded-lg" alt="The Alchemist">
-                <div class="mt-2 text-center">
-                    <h3 class="text-lg font-semibold">The Alchemist</h3>
-                    <p class="text-gray-500">Paulo Coelho</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6">
-            <div class="w-full flex flex-col items-center">
-                <img src="book.jpg" class="w-full aspect-[3/4] object-cover rounded-lg" alt="The Alchemist">
-                <div class="mt-2 text-center">
-                    <h3 class="text-lg font-semibold">The Alchemist</h3>
-                    <p class="text-gray-500">Paulo Coelho</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6">
-            <div class="w-full flex flex-col items-center">
-                <img src="book.jpg" class="w-full aspect-[3/4] object-cover rounded-lg" alt="The Alchemist">
-                <div class="mt-2 text-center">
-                    <h3 class="text-lg font-semibold">The Alchemist</h3>
-                    <p class="text-gray-500">Paulo Coelho</p>
-                </div>
-            </div>
-        </div>
+            </button>
+        </form>
 
     </div>
+
+    <div class="flex flex-wrap -m-3 bg-white rounded-xl shadow-lg ">
+        @foreach ($books as $book)
+            <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-6">
+                <div class="w-full flex flex-col items-center">
+                    <img src={{ $book->cover }} class="w-full aspect-[3/4] object-cover rounded-lg" alt="The Alchemist">
+                    <div class="mt-4 text-center">
+                        <h3 class="text-xl font-semibold">{{ $book->judul }}</h3>
+                        <p class="text-gray-500 ">{{ $book->penulis }}</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <button
+        class="bg-[#E5D5FC] p-3 rounded-full font-semibold shadow-lg hover:bg-[#F1E8FD] transition cursor-pointer absolute bottom-10 right-10">
+        <i data-feather="plus" class="w-8 h-8"></i>
+
+    </button>
 @endsection
