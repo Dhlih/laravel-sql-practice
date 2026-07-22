82c14a69aead2d3d73b069d07f1919adf87b2872
ifad <yusuffadhlih123@gmail.com>
2026-07-22 22:45:18 +0700

    added request is checking for showing bg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,34 +14,42 @@
         <div class="fixed top-0 left-0 max-w-[16rem] h-screen w-64 bg-white p-6 z-10">
             <h2 class="font-bold text-3xl">SiPustaka</h2>
             <ul class="flex flex-col gap-6 mt-8 ">
-                <li
-                    class="flex items-center gap-4 hover:bg-[#F1E8FD] p-2  rounded-lg cursor-pointer hover:text-purple-500 hover:font-semibold">
-                    <i data-feather="bar-chart-2"></i>
-                    <a href="dashboard">Dashboard</a>
+                <li>
+                    <a href="dashboard"
+                        class="flex items-center gap-4 hover:bg-[#F1E8FD] p-2  rounded-lg cursor-pointer hover:text-purple-500 hover:font-semibold {{ request()->is('dashboard') ? 'bg-[#F1E8FD] text-purple-500 font-semibold' : '' }}">
+                        <i data-feather="bar-chart-2"></i>
+                        <span>Dashboard</span>
+                    </a>
                 </li>
-                <li
-                    class="flex items-center gap-4 hover:bg-[#F1E8FD] p-2 rounded-lg cursor-pointer hover:text-purple-500 hover:font-semibold">
-                    <i data-feather="book"></i>
-                    <a href="buku">Buku</a>
+                <li>
+                    <a href="buku"
+                        class="flex items-center gap-4 hover:bg-[#F1E8FD] p-2  rounded-lg cursor-pointer hover:text-purple-500 hover:font-semibold {{ request()->is('buku') ? 'bg-[#F1E8FD] text-purple-500 font-semibold' : '' }}">
+                        <i data-feather="book-open"></i>
+                        <span>Buku</span>
+                    </a>
                 </li>
-                <li
-                    class="flex items-center gap-4 hover:bg-[#F1E8FD] p-2  rounded-lg cursor-pointer hover:text-purple-500 hover:font-semibold">
-                    <i data-feather="users"></i>
-                    <a href="anggota">Anggota</a>
+                <li>
+                    <a href="anggota"
+                        class="flex items-center gap-4 hover:bg-[#F1E8FD] p-2  rounded-lg cursor-pointer hover:text-purple-500 hover:font-semibold {{ request()->is('anggota') ? 'bg-[#F1E8FD] text-purple-500 font-semibold' : '' }}">
+                        <i data-feather="users"></i>
+                        <span>Anggota</span>
+                    </a>
                 </li>
-                <li
-                    class="flex items-center gap-4 hover:bg-[#F1E8FD] p-2  rounded-lg cursor-pointer hover:text-purple-500 hover:font-semibold">
-                    <i data-feather="refresh-ccw"></i>
-                    <a href="riwayat">Riwayat</a>
+                <li>
+                    <a href="riwayat"
+                        class="flex items-center gap-4 hover:bg-[#F1E8FD] p-2  rounded-lg cursor-pointer hover:text-purple-500 hover:font-semibold {{ request()->is('riwayat') ? 'bg-[#F1E8FD] text-purple-500 font-semibold' : '' }}">
+                        <i data-feather="refresh-ccw"></i>
+                        <span>Riwayat</span>
+                    </a>
                 </li>
             </ul>
             <div class="absolute bottom-0 left-0 w-full p-6 flex items-center gap-4">
                 <div
                     class="w-14 h-14 bg-purple-200 rounded-full flex justify-center items-center text-xl font-semibold">
-                    IY
+                    AD
                 </div>
                 <div>
-                    <p class="text-center font-semibold text-lg">Iyan Yudistira</p>
+                    <p class="text-center font-semibold text-lg">Admin</p>
                     <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
                         class="text-red-500  hover:underline">Keluar</a>
                     <form id="logout-form" action="/logout" method="POST" class="hidden">
