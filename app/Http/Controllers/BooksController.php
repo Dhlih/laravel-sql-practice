ab215ee1ff71c606967978132e71bf50ebd5c79d
ifad <yusuffadhlih123@gmail.com>
2026-07-23 16:43:07 +0700

    Menambahkan fungsi untuk menambahkan buku dari input yang dikirim halaman ke database

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -23,4 +23,18 @@ class BooksController extends Controller
     {
         return view("books.create");
     }
+
+    public function add_book(Request $request)
+    {
+        $judul = $request->input("judul");
+        $penulis = $request->input("penulis");
+        $deskripsi = $request->input("deskripsi");
+        $cover = $request->file("cover");
+
+        $file_name = time() . "_" . $cover->getClientOriginalName();
+        $cover_path = $cover->storeAs("public/covers", $file_name, "public");
+
+        DB::insert("INSERT INTO BOOKS (judul, penulis, deskripsi, cover) VALUES (?, ?, ?, ?)", [$judul, $penulis, $deskripsi, $cover_path]);
+        return redirect("/buku");
+    }
 }
