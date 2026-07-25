fe88ad2efd16a442637e75811d791dd58b4913bd
ifad <yusuffadhlih123@gmail.com>
2026-07-25 09:39:51 +0700

    Memubuat halaman untuk menampilkan detail buku

---
+++ resources/views/books/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Books')
+
+@section('content')
+    <h1 class="text-4xl font-bold text-gray-800">Detail Buku</h1>
+
+    <div class="flex flex-wrap -m-3 bg-white rounded-xl shadow-lg ">
+
+    </div>
+
+
+
+@endsection
