32033e686a9675282d53da3cd7448e044aededde
ifad <yusuffadhlih123@gmail.com>
2026-07-25 13:26:10 +0700

    Menghilangkan filter kategori dan menambahkan navigasi ke detail buku ketika gambar buku ditekan

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -10,26 +10,21 @@
             <input type="text" name="judul" placeholder="Ketik judul buku..."
                 class="w-full bg-white p-2 rounded-lg outline-none shadow-lg" value="{{ request('judul') }}">
 
-            <select class="bg-white p-2 rounded-lg outline-none shadow-lg">
-                <option value="test">Test</option>
-                <option value="test2">Test 2</option>
-                <option value="test3">Test 3</option>
-            </select>
-
             <button type="submit"
                 class="bg-[#F1E8FD] p-2 px-4 rounded-lg font-semibold shadow-lg hover:bg-[#E5D5FC] transition cursor-pointer">
                 <i data-feather="search"></i>
-
             </button>
         </form>
-
     </div>
 
     <div class="flex flex-wrap -m-3 bg-white rounded-xl shadow-lg ">
         @foreach ($books as $book)
             <div class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-6">
                 <div class="w-full flex flex-col items-center">
-                    <img src="{{ asset('storage/' . $book->cover) }}" class="w-full aspect-[3/4] object-cover rounded-lg" alt="{{ $book->judul }}">
+                    <a href="/buku/{{ $book->id }}">
+                        <img src="{{ asset('storage/' . $book->cover) }}"
+                            class="w-full aspect-[3/4] object-cover rounded-lg" alt="{{ $book->judul }}">
+                    </a>
                     <div class="mt-4 text-center">
                         <h3 class="text-xl font-semibold">{{ $book->judul }}</h3>
                         <p class="text-gray-500 ">{{ $book->penulis }}</p>
