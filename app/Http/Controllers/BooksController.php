a701b3c565e42cd8c8a1c26ef101f371a770b7f2
ifad <yusuffadhlih123@gmail.com>
2026-07-25 13:27:28 +0700

    Menambahkan fungsi untuk melihat detail buku dan mengedit buku

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -24,6 +24,42 @@ class BooksController extends Controller
         return view("books.create");
     }
 
+    public function show_book(string $id)
+    {
+
+        $book = DB::selectOne("SELECT id, judul, deskripsi, penulis, cover FROM BOOKS WHERE id = ?", [$id]);
+        if (!$book) {
+            abort(404);
+        }
+        return view("books.show", ["book" => $book]);
+    }
+
+    public function show_edit_book(string $id)
+    {
+        $book = DB::selectOne("SELECT id, judul, deskripsi, penulis, cover FROM BOOKS WHERE id = ?", [$id]);
+        return view("books.edit", ["book" => $book]);
+    }
+
+    public function edit_book(Request $request, string $id)
+    {
+        $old_book = DB::selectOne("SELECT judul, penulis, deskripsi, cover FROM BOOKS WHERE id =?", [$id]);
+
+        $judul = $request->input("judul") ?? $old_book->judul;
+        $penulis = $request->input("penulis") ?? $old_book->penulis;
+        $deskripsi = $request->input("deskripsi") ?? $old_book->deskripsi;
+        $cover = $request->file("cover");
+
+        if ($cover) {
+            $file_name = time() . "_" . $cover->getClientOriginalName();
+            $cover_path = $cover->storeAs("public/covers", $file_name, "public");
+        } else {
+            $cover_path = $old_book->cover;
+        }
+
+        DB::update("UPDATE BOOKS SET judul = ?, penulis = ?, deskripsi = ?, cover = ? WHERE id = ?", [$judul, $penulis, $deskripsi, $cover_path, $id]);
+        return redirect("/buku/$id");
+    }
+
     public function add_book(Request $request)
     {
         $judul = $request->input("judul");
