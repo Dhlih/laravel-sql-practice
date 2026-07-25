62eec3f47eef8c69d28a9badb1a7fbcf26c0a162
ifad <yusuffadhlih123@gmail.com>
2026-07-25 13:23:55 +0700

    Menambahkan endpoint dinamis untuk melihat detail buku dan mengedit detail buku

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 });
 
-# Books routes
+# Books static routes
 Route::get('/buku', [App\Http\Controllers\BooksController::class, 'show_books']);
 Route::get('/buku/tambah', [App\Http\Controllers\BooksController::class, 'show_create_book']);
 Route::post('/buku', [App\Http\Controllers\BooksController::class, 'add_book']);
+
+# Books dynamic routes
+Route::get('/buku/{id}', [App\Http\Controllers\BooksController::class, 'show_book']);
+Route::put('/buku/{id}', [App\Http\Controllers\BooksController::class, 'edit_book']);
+Route::get('/buku/{id}/edit', [App\Http\Controllers\BooksController::class, 'show_edit_book']);
+Route::get('/buku/{id}/edit', [App\Http\Controllers\BooksController::class, 'show_edit_book']);
+
