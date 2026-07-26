8dbd71f9f01cf6ec39bead17acd87da57495908d
ifad <yusuffadhlih123@gmail.com>
2026-07-26 09:39:52 +0700

    Menerapkan middleware authenticate dan redirectifauthenticate dan mengelompokkan route

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,37 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\RedirectIfAuthenticate;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('/login');
 });
 
-# Authentication routes
-Route::get('/login', [AuthController::class, "show_login"]);
-Route::post('/login', [AuthController::class, "login"]);
 Route::post('/logout', [AuthController::class, "logout"]);
 
-# Dashboard routes
-Route::get('/dashboard', function () {
-    return view('dashboard');
+
+Route::middleware([RedirectIfAuthenticate::class])->group(function () {
+    # Authentication routes
+    Route::get('/login', [AuthController::class, "show_login"]);
+    Route::post('/login', [AuthController::class, "login"]);
 });
 
-# Books static routes
-Route::get('/buku', [App\Http\Controllers\BooksController::class, 'show_books']);
-Route::get('/buku/tambah', [App\Http\Controllers\BooksController::class, 'show_create_book']);
-Route::post('/buku', [App\Http\Controllers\BooksController::class, 'add_book']);
+Route::middleware([Authenticate::class])->group(function () {
+    # Dashboard routes
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
 
-# Books dynamic routes
-Route::get('/buku/{id}', [App\Http\Controllers\BooksController::class, 'show_book']);
-Route::put('/buku/{id}', [App\Http\Controllers\BooksController::class, 'edit_book']);
-Route::get('/buku/{id}/edit', [App\Http\Controllers\BooksController::class, 'show_edit_book']);
-Route::get('/buku/{id}/edit', [App\Http\Controllers\BooksController::class, 'show_edit_book']);
+    # Books static routes
+    Route::get('/buku', [App\Http\Controllers\BooksController::class, 'show_books']);
+    Route::get('/buku/tambah', [App\Http\Controllers\BooksController::class, 'show_create_book']);
+    Route::post('/buku', [App\Http\Controllers\BooksController::class, 'add_book']);
 
+    # Books dynamic routes
+    Route::get('/buku/{id}', [App\Http\Controllers\BooksController::class, 'show_book']);
+    Route::put('/buku/{id}', [App\Http\Controllers\BooksController::class, 'edit_book']);
+    Route::get('/buku/{id}/edit', [App\Http\Controllers\BooksController::class, 'show_edit_book']);
+    Route::get('/buku/{id}/edit', [App\Http\Controllers\BooksController::class, 'show_edit_book']);
+});
